4efdcf4d85785d2e11d595464a65890ff0488af9
alvi <alimostakim68@gmail.com>
2023-08-26 16:39:14 +0600

    add the solution of problem1

---
+++ class3/problem1.py
@@ -0,0 +1,5 @@
+#Given the list fruits = ['apple','banana','cherry']. How can you add the item 'orange' to the end of the list?
+
+fruits = ['apple','banana','cherry']
+fruits.append('orange')
+print(fruits)
