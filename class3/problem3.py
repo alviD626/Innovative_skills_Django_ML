750cf8a9f4f675fa970f6de3430cbbefee518650
alvi <alimostakim68@gmail.com>
2023-08-26 22:52:55 +0600

    fix: the position problem has solved

--- class3/problem3.py
+++ class3/problem3.py
@@ -1,5 +1,5 @@
 # You have the list colors = ['red','green','blue']. How can you inset the item 'yellow' at the second position in the list?
 
 colors = ['red','green','blue']
-colors.insert(2,'yellow')
+colors.insert(1,'yellow')
 print(colors)
