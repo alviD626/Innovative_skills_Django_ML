9aec8da13fa377c4e22e6f808eedb9fd4d851263
alvi <alimostakim68@gmail.com>
2023-08-26 17:42:58 +0600

    add the solution of problem4

---
+++ class3/problem4.py
@@ -0,0 +1,5 @@
+# if you have the list letters = ['a','b','c','a','d'], how many occurrences of the letter 'a' are there in the list?
+
+letters = ['a','b','c','a','d']
+count = letters.count('a')
+print(count)
