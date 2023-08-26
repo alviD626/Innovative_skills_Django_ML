6204003c598698270ade7146f384f4a39cb81855
alvi <alimostakim68@gmail.com>
2023-08-26 18:14:27 +0600

    add the solution of problem7

---
+++ class3/problem7.py
@@ -0,0 +1,5 @@
+# you have the list cities = ['New York','Los Angeles','Chicago','Houston']. How can you find the index of the item 'Chicago'?
+
+cities = ['New York', 'Los Angeles','Chicago', 'Houston']
+index_item = cities.index('Chicago')
+print(index_item)
